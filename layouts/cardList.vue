1525c82c17636dea8671c3beed54573cddc10d22
AJIE SALAHUDIN PRIMA <ajiee.salahudin@gmail.com>
2023-05-26 15:24:54 +0700

    add example card

---
+++ layouts/cardList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <div class="bg-green-500 rounded-lg h-60 w-48">
+            <img src="https://getmyboat-user-images1.imgix.net/images/5afeaddc28bbb/boat-rentals-milna-splitsko-dalmatinska-zupanija-0-processed.jpg?auto=format%2Cenhance%2Credeye&fit=crop&h=None&ixlib=python-3.0.0&quality=80&w=None"
+                class="img-in-image-div">
+        </div>
+        <div class="info-box">
+            <div class="text-xs font-semibold mb-1">CROATIA</div>
+            <div class="text-base leading-5">Night time SUP Glow tour in Split</div>
+            <div class="text-base leading-5">From $47/person</div>
+            <div class="flex text-sm leading-5">
+                <i class="fas fa-star mr-1"></i>
+                <span class="text-sm leading-5">4.97 (429)</span>
+            </div>
+        </div>
+    </div>
+</template>
--- pages/example.vue
+++ pages/example.vue
@@ -2,6 +2,22 @@
     <div class="w-[1366px] m-auto flex justify-center bg-[url('~/assets/image/bghero.svg')] h-[750px] font-fontRoboto">
         <Header></Header>
     </div>
+    <div>
+        <div class="card w-[188px] h-[371px]">
+            <div class="rounded-lg h-60 w-48">
+                <img src="~/assets/image/imageOne.svg" class="img-in-image-div">
+            </div>
+            <div class="info-box font-fontRoboto">
+                <div class="font-bold text-xs leading-[1.2] uppercase text-secondaryGray">Cayman Islands</div>
+                <div class="font-fontRoboto font-bold text-base leading-[1.1875rem]">2 Night PAKAGE ALL Inclusive</div>
+                <div class="font-normal text-base leading-[1.5rem] text-secondaryDark">Rp 800.000 / malam</div>
+                <div class="flex text-sm leading-5 text-primaryGreeb">
+                    <div class="text-base leading-5">4.97</div>
+                    <span class="fas fa-star mr-1"> icon star</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
