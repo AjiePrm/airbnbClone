aa7e4e8f4197886e50f1a16372ff0f9abf72466e
AJIE SALAHUDIN PRIMA <ajiee.salahudin@gmail.com>
2023-06-01 09:31:39 +0700

    add nuxtLink

--- components/cardItem.vue
+++ components/cardItem.vue
@@ -3,30 +3,36 @@
         <div class="pb-6">
             <h1 class="font-fontRoboto font-semibold text-[24px] text-secondaryDark pb-2">{{ mainTitle }}
             </h1>
-            <h2 class="lg:w-[581px] w-[300px] font-fontRoboto font-light text-base leading-5 text-secondaryGray">{{ underTitle }}</h2>
+            <h2 class="lg:w-[581px] w-[300px] font-fontRoboto font-light text-base leading-5 text-secondaryGray">{{
+                underTitle }}</h2>
         </div>
         <div class="grid xl:grid-cols-6 gap-4 md:grid-cols-2 grid-cols-1  ">
             <div v-for="product in firstFiveProducts" :key="product.id" class="card w-[188px] h-[371px]">
-                <div class="rounded-lg w-[278px] h-[242px]  md:w-[188px] md:h-[240px] object-cover">
-                    <img :src="product.thumbnail"
-                        class="object-cover inline-block rounded-lg w-[188pxlx] h-[240px] img-in-image-div">
-                </div>
+                <nuxt-link :to="`/details/${product.id}`">
+                    <div class="rounded-lg w-[278px] h-[242px]  md:w-[188px] md:h-[240px] object-cover">
+                        <img :src="product.thumbnail"
+                            class="object-cover inline-block rounded-lg w-[188pxlx] h-[240px] img-in-image-div">
+                    </div>
 
-                <div class="font-fontRoboto py-[8px]">
-                    <div class="font-bold text-xs leading-[1.2] uppercase text-secondaryGray">{{ product.category }}</div>
-                    <div class="h-[38px] font-fontRoboto font-bold text-base leading-[1.1875rem] pb-2 pt-1 overflow-hidden">
-                        {{ product.title
+                    <div class="font-fontRoboto py-[8px]">
+                        <div class="font-bold text-xs leading-[1.2] uppercase text-secondaryGray">{{ product.category }}
+                        </div>
+                        <div
+                            class="h-[38px] font-fontRoboto font-bold text-base leading-[1.1875rem] pb-2 pt-1 overflow-hidden">
+                            {{ product.title
+                            }}
+                        </div>
+                        <div class="font-normal text-base leading-[1.5rem] text-secondaryGray pb-[5px]">$ {{ product.price
                         }}
+                        </div>
+                        <div class="flex text-sm leading-5 text-primaryGreeb">
+                            <div class="font-bold text-[12px] leading-[14px]">{{ product.rating }}</div>
+                            <span class="ml-1 ">
+                                <StarIcon class="h-[11px] w-[11px] text-blue-500" />
+                            </span>
+                        </div>
                     </div>
-                    <div class="font-normal text-base leading-[1.5rem] text-secondaryGray pb-[5px]">$ {{ product.price }}
-                    </div>
-                    <div class="flex text-sm leading-5 text-primaryGreeb">
-                        <div class="font-bold text-[12px] leading-[14px]">{{ product.rating }}</div>
-                        <span class="ml-1 ">
-                            <StarIcon class="h-[11px] w-[11px] text-blue-500" />
-                        </span>
-                    </div>
-                </div>
+                </nuxt-link>
             </div>
 
 
